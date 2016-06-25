ffa8c98d0cbe910689c017b9728ef05ccfb61e14
ckaksqh <ckaksqh@naver.com>
2016-06-26 04:08:36 +0900

    [Jong][Distance:100->25, LED ON-> LED OFF]

--- 01_ARDUINO/arduino/arduino.ino
+++ 01_ARDUINO/arduino/arduino.ino
@@ -3,15 +3,15 @@
 #define trigPin2 12
 #define echoPin2 11
 #define trigPin3 4
-#define echoPin3 3
+#define echoPin3  
 
 #define LEDPin1 13 // Onboard LED blue
 #define LEDPin2 6 // Onboard LED red
 #define LEDPin3 5 // Onboard LED grin
 
-int maximumRange = 200; // Maximum range needed
+int maximumRange = 25; // Maximum range needed
 int minimumRange = 0; // Minimum range needed 
-long duration, distance, BlueSensor,RedSensor,GrinSensor;
+long duration, duration2, distance, distance2, BlueSensor,RedSensor,GrinSensor;
 
 void setup()
 {
@@ -32,7 +32,7 @@ void loop() {
 SonarSensor(trigPin1, echoPin1);
 BlueSensor = distance;
 SonarSensor(trigPin2, echoPin2);
-RedSensor = distance;
+RedSensor = distance2;
 SonarSensor(trigPin3, echoPin3);
 GrinSensor = distance;
 Serial.println(BlueSensor);
@@ -42,37 +42,39 @@ Serial.println(GrinSensor);
 
 void SonarSensor(int trigPin,int echoPin)
 {
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2); 
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-distance = (duration/2) / 29.1;
+   digitalWrite(trigPin, LOW);
+   delayMicroseconds(2); 
+   digitalWrite(trigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin, LOW);
+   duration = pulseIn(echoPin, HIGH);
+   distance = (duration/2) / 29.1;
 
-if (distance >= maximumRange || distance <= minimumRange){
- /* Send a negative number to computer and Turn LED OFF 
- to indicate "out of range" */
-  Serial.println("-1");
-  digitalWrite(LEDPin1, LOW);
-  digitalWrite(LEDPin2, LOW);
-  digitalWrite(LEDPin3, LOW); 
- }
+   if (distance >= maximumRange || distance <= minimumRange){
+    /* Send a negative number to computer and Turn LED OFF 
+    to indicate "out of range" */
+     Serial.println("-1");
+     digitalWrite(LEDPin1, HIGH);
+     digitalWrite(LEDPin2, HIGH);
+     digitalWrite(LEDPin3, HIGH); 
+    }
 
- else{
- /* Send the distance to the computer using Serial protocol, and
- turn LED ON to indicate successful reading. */
-  Serial.println(distance);
-  Serial.println(BlueSensor);
-  Serial.println(RedSensor);
-  Serial.println(GrinSensor);
+    else{
+    /* Send the distance to the computer using Serial protocol, and
+    turn LED ON to indicate successful reading. */
+     Serial.println(distance);
+     Serial.println(BlueSensor);
+     Serial.println(RedSensor);
+     Serial.println(GrinSensor);
   
-  digitalWrite(LEDPin1, HIGH); 
-  digitalWrite(LEDPin2, HIGH);
-  digitalWrite(LEDPin3, HIGH);
- }
+     digitalWrite(LEDPin1, LOW); 
+     digitalWrite(LEDPin2, LOW);
+     digitalWrite(LEDPin3, LOW);
+     }
  
- //Delay 50ms before next reading.
- delay(100);
+    //Delay 50ms before next reading.
+    delay(100);
 }
+
+
 
